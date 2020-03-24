d7ac1d439181e34c8ad038be96661c99e58a3574
Jonas Aldén <50848757+jonasalden@users.noreply.github.com>
2020-03-24 18:27:11 +0100

    Module 3 Configure Middleware and Services

---
+++ Course/AspNetCore_03_MiddleWareServices/AspNetCore_03_MiddleWareServices/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCore_MiddleWareServices.Services;
+using System.Text;
+using System;
+
+namespace AspNetCore_03_MiddleWareServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Fields
+        readonly IPollResultsService _pollResults;
+        #endregion
+
+        #region Constructor
+        public HomeController(IPollResultsService pollResults)
+        {
+            _pollResults = pollResults;
+        }
+        #endregion
+
+        #region Methods
+        public IActionResult Index()
+        {
+            if (Request.Query.ContainsKey("submitted") == true)
+            {
+                var results = new StringBuilder();
+                var voteList = _pollResults.GetVoteResult();
+
+                foreach (var gameVotes in voteList)
+                {
+                    results.Append($"Game name: {gameVotes.Key}. Votes: {gameVotes.Value}{Environment.NewLine}");
+                }
+                return Content(results.ToString());
+            }
+            else
+            {
+                return Redirect("poll-questions.html");
+            }
+        }
+        #endregion
+    }
+}

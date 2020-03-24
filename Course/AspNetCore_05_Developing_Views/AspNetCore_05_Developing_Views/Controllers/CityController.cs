a83dd0d143138e407fe06436538204d707080a7a
Jonas Aldén <50848757+jonasalden@users.noreply.github.com>
2020-03-24 18:30:06 +0100

    Module 5 Developing Views

---
+++ Course/AspNetCore_05_Developing_Views/AspNetCore_05_Developing_Views/Controllers/CityController.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCore_05_Developing_Views.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_05_Developing_Views.Controllers
+{
+    public class CityController : Controller
+    {
+        #region Fields
+        private readonly ICityProvider _cities;
+        #endregion
+
+        #region Constructor
+        public CityController(ICityProvider cities)
+        {
+            _cities = cities;
+        }
+        #endregion
+
+        #region Methods
+        public IActionResult ShowCities()
+        {
+            ViewBag.Cities = _cities;
+            return View();
+        }
+
+        public IActionResult ShowDataForCity(string cityName)
+        {
+            ViewBag.City = _cities[cityName];
+            return View();
+        }
+
+        public IActionResult GetImage(string cityName)
+        {
+            return File($@"images\{cityName}.jpg", "image/jpeg");
+        }
+        #endregion
+    }
+}
---
+++ Course/AspNetCore_05_Developing_Views/AspNetCore_05_Developing_Views/Models/CityPopulation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_05_Developing_Views.Models
+{
+    public class CityPopulation
+    {
+        public int Year { get; }
+        public int City { get; }
+        public int Urban { get; }
+        public int Metro { get; }
+
+        #region Constructor
+        public CityPopulation(int year, int city, int urban, int metro)
+        {
+            Year = year;
+            City = city;
+            Urban = urban;
+            Metro = metro;
+        }
+        #endregion
+    }
+}

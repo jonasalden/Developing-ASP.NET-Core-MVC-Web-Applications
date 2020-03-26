c2af971ea06530c4fd92f40f4bf541e2f2aa63dc
Jonas Aldén <50848757+jonasalden@users.noreply.github.com>
2020-03-26 08:35:26 +0100

    Init Module 8
    Missing node_modules

---
+++ Course/AspNetCore_08_CssJsCore/AspNetCore_08_CssJsCore/Middleware/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+
+namespace AspNetCore_08_CssJsCore.Middleware
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseNodeModules(this IApplicationBuilder applicationBuilder, string root)
+        {
+            var path = Path.Combine(root, "node_modules");
+            var fileProvider = new PhysicalFileProvider(path);
+            var options = new StaticFileOptions
+            {
+                RequestPath = "/node_modules",
+                FileProvider = fileProvider
+            };
+
+            applicationBuilder.UseStaticFiles(options);
+
+            return applicationBuilder;
+
+        }
+    }
+}
